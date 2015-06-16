427207cd0313c0c4890bb74acd95c7a7a72a3ac4
Roderic Page <r.page@bio.gla.ac.uk>
2015-06-16 14:06:20 +0100

    Show web form by default

--- www/index.php
+++ www/index.php
@@ -12,6 +12,47 @@ $db->Connect("localhost",
 // Ensure fields are (only) indexed by column name
 $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
 
+//--------------------------------------------------------------------------------------------------
+function display_form()
+{
+	echo 
+'<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<style type="text/css" title="text/css">
+	body {
+		font-family: sans-serif;
+		margin:20px;
+		}
+</style>
+<title>Microcitation</title>
+</head>
+<body>
+<h1>Microcitation</h1>
+<p>Service to locate a microcitation</p>
+<form method="get" action=".">
+	ISSN<br>
+	<input type="text" name="issn" value="0019-1019">
+	<br>
+	Volume<br>
+	<input type="text" name="volume" value="99">
+	<br>
+	Page<br>
+	<input type="text" name="page" value="275">
+	<br>
+	Year<br>
+	<input type="text" name="year" value="">
+	<br>
+	Series<br>
+	<input type="text" name="series" value="">
+	<br>
+	<input type="submit" value="Go"></input>
+</form>
+</body>
+</html>';
+}
+
 
 //--------------------------------------------------------------------------------------------------
 function find ($issn, $volume, $page, $series='', $year = '')
@@ -40,7 +81,7 @@ function find ($issn, $volume, $page, $series='', $year = '')
 	 
 	if (isset($obj->volume))
 	{
-		$sql .= 'AND volume=' . $volume;
+		$sql .= ' AND volume=' . $volume;
 	}
 	 
 	$sql .= ' AND (' . $page . ' between spage and epage)';
@@ -60,53 +101,76 @@ function find ($issn, $volume, $page, $series='', $year = '')
 	$result = $db->Execute($sql);
 	if ($result == false) die("failed [" . __LINE__ . "]: " . $sql);
 	
-	if ($result->NumRows() == 1)
+	$obj->results = array();
+	
+	while (!$result->EOF) 
 	{
-		$obj->found = true;
 		if ($result->fields['doi'])
 		{
-			$obj->doi = $result->fields['doi'];
+			$obj->results[] = $result->fields['doi'];
 		}
+		
+		$result->MoveNext();
 	}
+	
+	$obj->found = count($obj->results) > 0;
 	
 	return $obj;
 
 }
 
 
-$issn = '';
-$volume = '';
-$page = '';
-$year = '';
-$series = '';
-
-if (isset($_GET['issn']))
-{
-	$issn = $_GET['issn'];
-}
-if (isset($_GET['volume']))
-{
-	$volume = $_GET['volume'];
-}
-if (isset($_GET['page']))
-{
-	$page = $_GET['page'];
-}
-if (isset($_GET['year']))
-{
-	$year = $_GET['year'];
-}
-if (isset($_GET['series']))
-{
-	$series = $_GET['series'];
-}
 
 
-$obj = find($issn, $volume, $page, $series, $year);
 
-header("Content-Type:text/plain");
-echo json_format(json_encode($obj));
 
-//print_r($obj);
+function main()
+{
+	$text = '';
+	$format = '';
+	if (isset($_GET['issn']) 
+		&& (isset($_GET['volume']) || isset($_GET['year']))
+		&& isset($_GET['page']))
+	{
+		$issn = '';
+		$volume = '';
+		$page = '';
+		$year = '';
+		$series = '';
+
+		if (isset($_GET['issn']))
+		{
+			$issn = $_GET['issn'];
+		}
+		if (isset($_GET['volume']))
+		{
+			$volume = $_GET['volume'];
+		}
+		if (isset($_GET['page']))
+		{
+			$page = $_GET['page'];
+		}
+		if (isset($_GET['year']))
+		{
+			$year = $_GET['year'];
+		}
+		if (isset($_GET['series']))
+		{
+			$series = $_GET['series'];
+		}	
+		
+		$obj = find($issn, $volume, $page, $series, $year);
+
+		header("Content-Type:text/plain");
+		echo json_format(json_encode($obj));
+	}
+	else
+	{
+		display_form();
+	}
+}
+	
+main();
+
 
 ?>
